3b05fc1241dfcfa11f67a121c8dcfa2878460d82
Nerses <knersesk@gmail.com>
2022-07-09 22:06:46 +0300

    updated admin

--- scraping/admin.py
+++ scraping/admin.py
@@ -5,10 +5,4 @@ admin.site.register(City)
 admin.site.register(Language)
 admin.site.register(Error)
 admin.site.register(Url)
-
-class VacancyAdmin(admin.ModelAdmin):
-    ist_display = ('id', 'title', 'language', 'city', 'timestamp')
-    list_display_links = ('id', 'title')
-    search_fields = ('title','language', 'city',)
-
-admin.site.register(Vacancy,VacancyAdmin)
+admin.site.register(Vacancy)
